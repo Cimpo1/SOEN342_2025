16a57d3af213c4834e723ecc970ec9b151f90ef5
thekillerturkey <70587573+thekillerturkey@users.noreply.github.com>
2025-10-24 20:33:53 -0400

    Artefact final touches
    Updated uml class diagram, domain model, as well as created remaining sequence diagrams and created all operation contracts for them.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
+       
         Terminal t = new Terminal();
-        
         t.accessTerminal();
     }
 }
